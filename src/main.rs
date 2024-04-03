6551dfeccb4001edc02ee43b49547df4d42502ae
David Campbell <dcampbell24@gmail.com>
2024-04-02 20:34:23 -0400

    WIP

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 use std::net::{Ipv4Addr, TcpStream};
-use std::sync::mpsc::channel; //, RecvTimeoutError};
+use std::sync::mpsc::{channel, Sender}; //, RecvTimeoutError};
 use std::thread;
 use std::time::{Duration, Instant};
 use std::{io, net::SocketAddrV4};
 
-use gpsd_proto::{get_data, ResponseData};
+use gpsd_proto::{get_data, handshake, GpsdError, ResponseData};
 use metrics::gauge;
 use metrics_exporter_prometheus::PrometheusBuilder;
 
@@ -18,39 +18,15 @@ fn main() {
     let _satellites = gauge!("satellites");
 
     let (tx, rx) = channel();
-
-    thread::spawn(move || {
-        if let Ok(stream) = TcpStream::connect("127.0.0.1:2947") {
+    if let Ok(stream) = TcpStream::connect("127.0.0.1:2947") {
+        thread::spawn(move || {
             let mut reader = io::BufReader::new(&stream);
-            let mut _writer = io::BufWriter::new(&stream);
-
-            let mut reader: &mut dyn io::BufRead = &mut reader;
-            let mut _writer: &mut io::BufWriter<dyn std::io::Write> = &mut _writer;
-
-            let mut count = 0;
-
-            match get_data(&mut reader) {
-                Ok(data) => println!("{data:?}"),
-                Err(err) => println!("{err}"),
-            }
-
-            loop {
-                let msg = get_data(&mut reader).unwrap();
-                if let ResponseData::Sky(sky) = msg {
-                    count = sky.satellites.map_or_else(
-                        || 0,
-                        |sats| sats.iter().filter(|sat| sat.used).map(|_| 1).sum(),
-                    );
-                    println!("{count}");
-                    tx.send(count).expect("Unable to send on channel");
-                } else {
-                    println!("... {count}")
-                }
-            }
-        } else {
-            panic!("Couldn't connect to gpsd...");
-        }
-    });
+            let mut writer = io::BufWriter::new(&stream);
+            demo_forever(tx, &mut reader, &mut writer).unwrap();
+        });
+    } else {
+        panic!("Couldn't connect to gpsd...");
+    }
 
     loop {
         let timeout = Duration::from_millis(10_000);
@@ -61,12 +37,41 @@ fn main() {
             match rx.recv_timeout(timeout - t0.elapsed()) {
                 Ok(count_) => count = count_,
                 Err(_) => {
-                    println!("...");
+                    println!("*...*");
                     break;
                 }
             }
         }
 
-        println!("{count}")
+        println!("*{count}*")
     }
+}
+
+pub fn demo_forever<R>(
+    tx: Sender<i32>,
+    mut reader: &mut dyn io::BufRead,
+    writer: &mut io::BufWriter<R>,
+) -> Result<(), GpsdError>
+where
+    R: std::io::Write,
+{
+
+        let mut count = 0;
+
+        handshake(reader, writer).unwrap();
+
+        loop {
+            let msg = get_data(&mut reader).unwrap();
+            if let ResponseData::Sky(sky) = msg {
+                count = sky.satellites.map_or_else(
+                    || 0,
+                    |sats| sats.iter().filter(|sat| sat.used).map(|_| 1).sum(),
+                );
+                println!("{count}");
+                tx.send(count).expect("Unable to send on channel");
+            } else {
+                println!("... {count}")
+            }
+        }
+
 }
